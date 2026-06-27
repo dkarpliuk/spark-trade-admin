081be1cb030bae2a16223476ef39012a9258bf90
Dmytro Karpliuk <d.karpljyk@gmail.com>
2026-06-28 00:35:40 +0700

    Disable query retries globally

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchOnWindowFocus: false },
+    queries: { refetchOnWindowFocus: false, retry: false },
   },
 })
 
