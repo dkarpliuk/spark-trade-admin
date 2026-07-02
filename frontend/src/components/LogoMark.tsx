4bcda4fc145565e5717b417991b4b58820d85365
Dmytro Karpliuk <d.karpljyk@gmail.com>
2026-07-02 14:32:03 +0700

    feat: replace header logo and favicon with new brand assets

--- frontend/src/components/LogoMark.tsx
+++ frontend/src/components/LogoMark.tsx
@@ -1,4 +1,4 @@
-import logoMark from '@/assets/logo-mark.svg'
+import logoMark from '@/assets/logo-mark.png'
 
 function LogoMark() {
   return <img src={logoMark} alt="Spark.trade Admin" className="size-8 select-none" />
